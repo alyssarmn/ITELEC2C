26046bc67ac062ec303506445f4ba06876b582f9
alyssarmn <alyssamarie.romen.cics@ust.edu.ph>
2024-02-02 23:55:52 +0800

    COMMIT

---
+++ replicate-ui/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Header from "./Header";
+import Gallery from "./Gallery";
+import Footer from "./Footer";
+// import Gallery from "./Gallery";
+// import Sns from "./Sns";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Gallery />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ replicate-ui/src/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <center>
+      <div className="footer-container">
+        <div className="footer-imgcontainer">
+          <img
+            className="footer-image"
+            src="./logo_pledis.png"
+            alt="Logo"
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </div>
+    </center>
+  );
+};
+
+export default Footer;
